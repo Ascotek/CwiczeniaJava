b9399d611fb25c91e84a3e058fec6c5ee50978af
Ascotek <paulinatochwin@gmail.com>
2019-06-06 15:04:58 +0200

    dodanie nowego folderu

---
+++ Fabryka.java
@@ -0,0 +1,11 @@
+package pl.com.dziendobry.DodatkoweCwiczenia;
+
+public class Fabryka {
+    public static void main(String[] args) {
+        CircleEx kolo = new CircleEx(5);
+        Rectangle prostokat = new Rectangle(5,8);
+        TriangleEx trojkat = new TriangleEx(5,8,5, 8);
+    }
+
+
+}
